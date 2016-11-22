da59c2a91941d3359a5b9f66f047350d91de904d
Alberto Garcia Fernandez <agarcia@idealista.com>
2016-11-22 12:09:26 +0100

    Make use of systemd

--- tests/test_ansible.py
+++ tests/test_ansible.py
@@ -1,17 +1,21 @@
 import pytest
 
+
 @pytest.fixture()
 def AnsibleDefaults(Ansible):
     return Ansible("include_vars", "defaults/main.yml")["ansible_facts"]
+
 
 @pytest.fixture()
 def AnsibleVarBinDir(Ansible):
     return Ansible("debug", "msg={{ node_exporter_bin_dir }}")["msg"]
 
+
 def test_node_exporter_user(User, Group, AnsibleDefaults):
     assert User(AnsibleDefaults["node_exporter_user"]).exists
     assert Group(AnsibleDefaults["node_exporter_group"]).exists
     assert User(AnsibleDefaults["node_exporter_user"]).group == AnsibleDefaults["node_exporter_group"]
+
 
 def test_node_exporter_conf(File, User, Group, AnsibleDefaults):
     conf_dir = File(AnsibleDefaults["node_exporter_root_dir"])
@@ -19,6 +23,7 @@ def test_node_exporter_conf(File, User, Group, AnsibleDefaults):
     assert conf_dir.is_directory
     assert conf_dir.user == AnsibleDefaults["node_exporter_user"]
     assert conf_dir.group == AnsibleDefaults["node_exporter_group"]
+
 
 def test_node_exporter_executable(File, Command, AnsibleDefaults, AnsibleVarBinDir):
     node_exporter = File(AnsibleDefaults["node_exporter_bin_dir"] + "/node_exporter")
@@ -33,11 +38,13 @@ def test_node_exporter_executable(File, Command, AnsibleDefaults, AnsibleVarBinD
     assert node_exporter_version.rc is 0
     assert "version " + AnsibleDefaults["node_exporter_version"] in node_exporter_version.stdout
 
+
 def test_node_exporter_service(File, Service, Socket, AnsibleDefaults):
-    assert File("/etc/init.d/node_exporter").exists
+    assert File("/etc/systemd/system/node_exporter.service").exists
     assert Service("node_exporter").is_enabled
     assert Service("node_exporter").is_running
     assert Socket("tcp://:::9100").is_listening
+
 
 def test_node_exporter_roles(File, AnsibleDefaults):
     roles = File(AnsibleDefaults["node_exporter_textfile_collector"] + "/roles.prom")
